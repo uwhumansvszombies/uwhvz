88a719d2f41125cfbeddca3e73f1deeadbfea31f
Tiffany Yeung <tiffanynwyeung@gmail.com>
2017-12-09 22:25:13 -0500

    Some initial basic styling project layout and structure

--- app/views.py
+++ app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def dashboard(request):
+    return render(request, 'dashboard/dashboard.html')
