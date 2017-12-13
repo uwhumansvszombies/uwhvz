193264ceb2c2a27edc7c7180d93a29138dd7db73
Tiffany Yeung <tiffanynwyeung@gmail.com>
2017-12-13 11:23:19 -0500

    redirect to dashboard after login, block access if not logged in

--- app/views.py
+++ app/views.py
@@ -5,4 +5,7 @@ def index(request):
     return render(request, 'index.html')
 
 def dashboard(request):
-    return render(request, 'dashboard/dashboard.html')
+    if (request.user.is_authenticated):
+        return render(request, 'dashboard/dashboard.html')
+    else:
+        return render(request, 'registration/login.html')
