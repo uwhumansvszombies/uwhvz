96aa99292306265b2b53dedd2f9ed155b6114715
Tristan Ohlson <tsohlson@gmail.com>
2018-03-19 01:07:14 -0400

    Introduce moderator pages. Create a player list page (#7)

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import include, path
 from . import views
 
-
 urlpatterns = [
     path('', views.index, name='index'),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('moderator/players', views.player_list, name='player list'),
 ]
