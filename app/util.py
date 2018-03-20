de2dc8cdccbcb120adf7d32a0395bfbef79c6134
Tristan Ohlson <tohlson@squareup.com>
2018-03-20 18:20:56 -0400

    Allow superusers to be moderators by default

--- app/util.py
+++ app/util.py
@@ -1,4 +1,4 @@
 def is_moderator(user):
-    if user.groups.filter(name='Moderator').count() > 0:
+    if user.groups.filter(name='Moderator').count() > 0 or user.is_superuser:
         return True
     return False
