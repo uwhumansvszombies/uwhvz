b9719a99716126f2570ad8a75a23b7c2873d6795
Tiffany Yeung <tiffanynwyeung@gmail.com>
2018-03-20 20:47:19 -0400

    collapse moderator pages into dashboard with a dash of sketchy stuff

--- app/views.py
+++ app/views.py
@@ -17,17 +17,17 @@ def dashboard(request):
 @user_passes_test(is_moderator)
 def player_list(request):
     players = Player.objects.all()
-    return render(request, 'moderator/player_list.html', {'players': players})
+    return render(request, 'dashboard/player_list.html', {'players': players})
 
 
 @user_passes_test(is_moderator)
 def add_player(request):
     if request.method == 'POST':
         print("post!!!")
-    return render(request, 'moderator/add_player.html')
+    return render(request, 'dashboard/add_player.html')
 
 
 @user_passes_test(is_moderator)
 def signup_locations(request):
     locations = SignupLocation.objects.all()
-    return render(request, 'moderator/signup_locations.html', {'signup_locations': locations})
+    return render(request, 'dashboard/signup_locations.html', {'signup_locations': locations})
