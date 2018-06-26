9437451fb0b0d27f699418a0d7b6a2d7976bf392
Tristan Ohlson <tsohlson@gmail.com>
2018-06-26 01:09:09 -0400

    This commit adds user agent based mobile vs desktop templates

--- app/views/signup.py
+++ app/views/signup.py
@@ -2,11 +2,12 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.db import transaction
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.utils import timezone
+from django.utils.decorators import method_decorator
 
 from app.models import SignupToken, User, Game, Player, PlayerRole
-from app.util import require_post_parameters
+from app.util import require_post_parameters, MobileSupportedView
 
 
 def signup(request, signup_token):
@@ -17,13 +18,26 @@ def signup(request, signup_token):
         return redirect('user_signup', signup_token=signup_token)
 
 
-def user_signup(request, signup_token):
-    token = SignupToken.objects.get(pk=signup_token)
-    if token.used_at:
-        messages.add_message(request, messages.INFO, f'You\'ve already created an account using {token.email}.')
-        return redirect('dashboard')
+@method_decorator(login_required, name='dispatch')
+class UserSignupView(MobileSupportedView):
+    desktop_template = 'dashboard/user_signup.html'
+    mobile_template = 'dashboard/user_signup.html'
 
-    if request.method == 'POST':
+    def get(self, request):
+        signup_token = self.self.kwargs['signup_token']
+        token = SignupToken.objects.get(pk=signup_token)
+        if token.used_at:
+            messages.add_message(request, messages.INFO, f'You\'ve already created an account using {token.email}.')
+            return redirect('dashboard')
+
+        return self.mobile_or_desktop(request, {'signup_token': signup_token})
+
+    def post(self, request, signup_token):
+        token = SignupToken.objects.get(pk=signup_token)
+        if token.used_at:
+            messages.add_message(request, messages.INFO, f'You\'ve already created an account using {token.email}.')
+            return redirect('dashboard')
+
         first_name, last_name, password = require_post_parameters(request, 'first_name', 'last_name', 'password')
         with transaction.atomic():
             User.objects.create_user(token.email, password, first_name=first_name, last_name=last_name)
@@ -34,17 +48,19 @@ def user_signup(request, signup_token):
         login(request, user)
         return redirect('game_signup')
 
-    return render(request, 'dashboard/user_signup.html', {'signup_token': signup_token})
 
+@method_decorator(login_required, name='dispatch')
+class GameSignupView(MobileSupportedView):
+    desktop_template = 'dashboard/game_signup.html'
+    mobile_template = 'dashboard/game_signup.html'
 
-@login_required
-def game_signup(request):
-    if request.method == 'POST':
+    def get(self, request):
+        games = Game.objects.all()
+        return self.mobile_or_desktop(request, {'games': games})
+
+    def post(self, request):
         game_id, = require_post_parameters(request, 'game')
         in_oz_pool = request.POST.get('is_oz', 'off') == 'on'
         game = Game.objects.get(pk=game_id)
         Player.objects.create_player(request.user, game, PlayerRole.HUMAN, in_oz_pool=in_oz_pool)
         return redirect('dashboard')
-
-    games = Game.objects.all()
-    return render(request, 'dashboard/game_signup.html', {'games': games})
--- app/views/views.py
+++ app/views/views.py
@@ -1,31 +1,37 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.utils import dateparse
+from django.utils.decorators import method_decorator
 
 from app.models import Player, Tag, Game
-from app.util import require_post_parameters
+from app.util import require_post_parameters, MobileSupportedView
 
 
-def index(request):
-    return render(request, 'index.html')
+class IndexView(MobileSupportedView):
+    desktop_template = 'index.html'
+    mobile_template = 'index.html'
 
 
-@login_required
-def dashboard(request):
-    try:
-        player = request.user.player_set.get()
-    except ObjectDoesNotExist:
-        messages.add_message(request, messages.INFO, "You haven't signed up for any games yet.")
-        return redirect('game_signup')
-    team_score = sum([p.score() for p in Player.objects.all()])
-    return render(request, 'dashboard/index.html', {'player': player, 'team_score': team_score})
+@method_decorator(login_required, name='dispatch')
+class DashboardView(MobileSupportedView):
+    desktop_template = 'dashboard/index.html'
+    mobile_template = 'dashboard/index.html'
+
+    def get(self, request):
+        try:
+            player = request.user.player_set.get()
+        except ObjectDoesNotExist:
+            messages.add_message(request, messages.INFO, "You haven't signed up for any games yet.")
+            return redirect('game_signup')
+        team_score = sum([p.score() for p in Player.objects.all()])
+        return self.mobile_or_desktop(request, {'player': player, 'team_score': team_score})
 
 
-@login_required
-def report_tag(request):
-    if request.method == 'POST':
+@method_decorator(login_required, name='dispatch')
+class ReportTagView(MobileSupportedView):
+    def post(self, request):
         game_id, player_code, date, time, location, description = \
             require_post_parameters(request, 'game', 'player_code', 'date', 'time', 'location', 'description')
         game = Game.objects.get(pk=game_id)
@@ -36,5 +42,7 @@ def report_tag(request):
 
         Tag.objects.create_tag(initiating_player, receiving_player, datetime, location, description)
         messages.add_message(request, messages.INFO, f'Reported a tag on {receiving_player.user.get_full_name()}')
+        return redirect('dashboard')
 
-    return redirect('dashboard')
+    def get(self, request):
+        return redirect('dashboard')
