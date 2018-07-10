4542dd0a3ec999ed45ffd971e5dac981b064ec99
Tristan Ohlson <tsohlson@gmail.com>
2018-07-10 19:50:39 -0400

    Redirect from /dashboard/ to /dashboard in wagtail

--- app/models/wagtail/root.py
+++ app/models/wagtail/root.py
@@ -1,3 +1,4 @@
+from django.shortcuts import redirect
 from wagtail.core.models import Page
 
 
@@ -8,3 +9,9 @@ class RootPage(Page):
 class DashboardPage(Page):
     subpage_types = ['app.GameInfoPage', 'app.NewsPage']
     parent_page_types = ['app.RootPage']
+
+    def serve(self, request, **kwargs):
+        # Django routes to this for /dashboard/ but not /dashboard.
+        # To save some server errors about DashboardPage not having
+        # a template we will just redirect to dashboard.
+        return redirect('dashboard')
