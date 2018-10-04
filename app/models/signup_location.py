f38bfb923ad0b68868905143f60786446d27e3a7
Tiffany Yeung <tiffanynwyeung@gmail.com>
2018-10-04 16:40:50 -0400

    add game to signup locations

--- app/models/signup_location.py
+++ app/models/signup_location.py
@@ -3,6 +3,8 @@ from datetime import datetime
 
 from django.db import models
 
+from .game import Game
+
 
 class SignupLocationManager(models.Manager):
     def create_signup_location(self, name: str) -> 'SignupLocation':
@@ -13,6 +15,7 @@ class SignupLocationManager(models.Manager):
 
 class SignupLocation(models.Model):
     id: uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    game: Game = models.ForeignKey(Game, on_delete=models.PROTECT)
     name: str = models.CharField(max_length=150, unique=True)
 
     created_at: datetime = models.DateTimeField(auto_now_add=True)
