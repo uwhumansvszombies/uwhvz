1507145026422ece45fd64ff852ca5400f9240e8
UW HvZ <40743370+uwhvz@users.noreply.github.com>
2019-12-11 19:25:35 -0500

    Update middleware.py

--- app/middleware.py
+++ app/middleware.py
@@ -1,6 +1,12 @@
 from app.models import *
 
 class ImpersonateMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        return self.get_response(request)
+    
     def process_request(self, request):
         if request.user.is_superuser and "__impersonate" in request.GET:
             request.session['impersonate_id'] = int(request.GET["__impersonate"])
