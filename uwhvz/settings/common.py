4830304e65d1a92c2da46eb01504a2e142f0cc9f
UW HvZ <40743370+uwhvz@users.noreply.github.com>
2019-12-11 21:36:40 -0500

    Impersonation selection menu

--- uwhvz/settings/common.py
+++ uwhvz/settings/common.py
@@ -13,6 +13,7 @@ if not os.path.exists(MEDIA_DIR):
 
 INSTALLED_APPS = [
     'app',
+    'ajax_select',
     'bootstrap3_datetime',
     'django_su',  # must be before ``django.contrib.admin``    
     'django.contrib.admin',
