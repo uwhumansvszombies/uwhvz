f92de1aec5afd9e46c8a2ebf2bbe10a354295db9
UW HvZ <40743370+uwhvz@users.noreply.github.com>
2019-12-29 13:31:01 -0700

    Update legacy.py

--- app/models/legacy.py
+++ app/models/legacy.py
@@ -7,15 +7,22 @@ from .user import User
 
 
 class LegacyManager(models.Manager):
-    def create_legacy(self, user: User, cost: int, details: str) -> 'Legacy':
-        legacy = self.model(user=user, cost=cost, details=details)
+    def create_legacy(self, user: User, value: int, details: str) -> 'Legacy':
+        legacy = self.model(user=user, value=value, details=details)
         legacy.save()
         return legacy
 
 
 class Legacy(models.Model):
+    # This model is used to store gains or losses of legacy points
+    # If a player has no legacys, they have a value of zero implicitly
+    
     id: uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    cost: int = models.IntegerField()
+    value: int = models.IntegerField()
+    
+    # this is the amount gained or lost by the user
+    # a negative value implies losing points, a positive value implies gaining points
+    
     details: str = models.CharField(max_length=50)
     
     user: User = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_legacy')
@@ -28,4 +35,4 @@ class Legacy(models.Model):
     objects = LegacyManager()
 
     def __int__(self):
-        return self.cost
+        return self.value
